854fa0971d574a929c6c21897910edf6be408994
Christian Zogbi <christianz@lagash.com>
2019-08-25 10:40:38 -0300

    Actualizacion estructura de datos

--- Estructura datos Imppa.sql
+++ Estructura datos Imppa.sql
@@ -1,30 +1,27 @@
---Tabla Usuarios
-CREATE TABLE [Usuarios]
+CREATE TABLE [Titulo]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
-	[TipoUsuario] INT FOREIGN KEY REFERENCES TipoUsuario(ID) NOT NULL,
-	[Mail] VARCHAR(50) NOT NULL,
-	[Contraseña] VARCHAR(10) NOT NULL,
-	[AddedDate] DATETIME NOT NULL,
-	[LastLogin] DATETIME NOT NULL,
-	[Nombre] varchar(50) NULL,
-	[Apellido] VARCHAR(50) NULL,
-	[Direccion] VARCHAR MAX
+	[Titulo] BINARY NOT NULL
 )
 
---Tabla Tipo Usuario
+CREATE TABLE [Foto]
+(
+	[ID] INT IDENTITY PRIMARY KEY,
+	[Foto] BINARY NOT NULL
+)
+
+--Tabla Tipo Clases
+CREATE TABLE [TipoClase]
+(
+	[ID] INT IDENTITY PRIMARY KEY,
+	[Tipo] VARCHAR(50)
+)
+
+----Tabla Tipo Usuario
 CREATE TABLE [TipoUsuario]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
 	[Tipo] VARCHAR MAX NOT NULL
-)
-
---Tabla Tipo Usuario
-CREATE TABLE [TituloUsuario]
-(
-	[ID] INT IDENTITY PRIMARY KEY,
-	[IDUsuario] INT FOREIGN KEY REFERENCES Usuario(ID)NOT NULL,
-	[IDTitulo] FOREIGN KEY REFERENCES Titulo(ID)NOT NULL
 )
 
 CREATE TABLE [Titulo]
@@ -33,41 +30,62 @@ CREATE TABLE [Titulo]
 	[Titulo] BINARY NOT NULL
 )
 
+go
 
---Tabla Subscripcion Usuario
-CREATE TABLE [SubscripcionUsuario]
+----Tabla Usuarios
+CREATE TABLE [Usuarios]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
-	[IDUsuario] INT FOREIGN KEY REFERENCES Usuario(ID)NOT NULL,
-	[IDSubscripcion] FOREIGN KEY REFERENCES [Subscripcion](ID)NOT NULL
+	[TipoUsuario] INT FOREIGN KEY REFERENCES TipoUsuario(ID) NOT NULL,
+	[Mail] VARCHAR(50) NOT NULL,
+	[Contrasenia] VARCHAR(10) NOT NULL,
+	[AddedDate] DATETIME NOT NULL,
+	[LastLogin] DATETIME NOT NULL,
+	[Nombre] varchar(50) NULL,
+	[Apellido] VARCHAR(50) NULL,
+	[Direccion] VARCHAR MAX
+	[Habilitado] bit not null,
 )
+
 --Tabla Subcripciones
 CREATE TABLE [Subscripcion]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
 	[Nombre] VARCHAR(50) NOT NULL,
-	[Descripcion] VARCHAR MAX NULL,
+	[Descripcion] VARCHAR (MAX) NULL,
 	[Precio] INT NOT NULL
 )
+
 
 --Tabla Comentarios
 CREATE TABLE [Comentarios]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
-	[IDProfesor] INT FOREIGN KEY REFERENCES Usuario(ID) NOT NULL,
-	[IDAlumno] INT FOREIGN KEY REFERENCES Usuario(ID) NOT NULL,
-	[Comentario] VARCHAR MAX NOT NULL,
-	[Puntaje] VARCHAR MAX NOT NULL
+	[IDProfesor] INT FOREIGN KEY REFERENCES Usuarios(ID) NOT NULL,
+	[IDAlumno] INT FOREIGN KEY REFERENCES Usuarios(ID) NOT NULL,
+	[Comentario] VARCHAR (MAX) NOT NULL,
+	[Puntaje] VARCHAR (MAX) NOT NULL
 )
+
+go
 
 --Tabla Clases
 CREATE TABLE [ClaseProfesor]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
-	[IDUsuario] INT FOREIGN KEY REFERENCES Usuario(ID) NOT NULL,
+	[IDUsuario] INT FOREIGN KEY REFERENCES Usuarios(ID) NOT NULL,
 	[IDCategoriaClase] INT FOREIGN KEY REFERENCES CategoriaClase(ID) NOT NULL,
 	[IDTipoClase] INT FOREIGN KEY REFERENCES TipoClase(ID) NOT NULL,
 	[Precio] INT NOT NULL
+)
+
+
+--Tabla Subscripcion Usuario
+CREATE TABLE [SubscripcionUsuario]
+(
+	[ID] INT IDENTITY PRIMARY KEY,
+	[IDUsuario] INT FOREIGN KEY REFERENCES Usuarios(ID)NOT NULL,
+	[IDSubscripcion] INT FOREIGN KEY REFERENCES [Subscripcion](ID)NOT NULL
 )
 
 
@@ -83,4 +101,12 @@ CREATE TABLE [TipoClase]
 (
 	[ID] INT IDENTITY PRIMARY KEY,
 	[Tipo] VARCHAR(50)
+)
+
+--Tabla Tipo Usuario
+CREATE TABLE [TituloUsuario]
+(
+	[ID] INT IDENTITY PRIMARY KEY,
+	[IDUsuario] INT FOREIGN KEY REFERENCES Usuarios(ID)NOT NULL,
+	[IDTitulo] INT FOREIGN KEY REFERENCES Titulo(ID)NOT NULL
 )
