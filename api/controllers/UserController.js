291172c149077c33468552ebfbc593e54e249c19
Christian Zogbi <christianz@lagash.com>
2019-08-28 17:10:44 -0300

    ABM Usuarios

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,12 +1,31 @@
+var express = require('express');
 var UserService  = require('../services/UserService');
 
  exports.getUsers = function(req, res) {
-   console.log("llego al controller"); 
-   var data = UserService.getUsers(req, res);
+   console.log(req.body);
+   if(req.body.UserId != null)
+   {
+      var data = UserService.getUser(req, res);
+   }
+   else
+   {
+      var data = UserService.getUsers(req, res);
+   }
+   
    return(data);
  };
- 
+
  exports.addUser = function(req, res) {
     console.log(req.body);
     return(UserService.addUser(req, res));
+};
+
+exports.updateUser = function(req, res) {
+  console.log(req.body);
+  return(UserService.updateUser(req, res));
+};
+
+exports.deleteUser = function(req, res) {
+  console.log(req.body);
+  return(UserService.deleteUser(req, res));
 };
