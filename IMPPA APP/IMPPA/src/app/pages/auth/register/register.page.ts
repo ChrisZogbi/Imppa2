2a387aec289f8685bc47ea69269b1c4552258c6e
martinzb <zbinden.d.m@gmail.com>
2019-10-18 19:29:48 -0300

    agrego modulo

--- IMPPA APP/IMPPA/src/app/pages/auth/register/register.page.ts
+++ IMPPA APP/IMPPA/src/app/pages/auth/register/register.page.ts
@@ -1,12 +1,10 @@
-import { SignupStep1Component } from './../../../components/signup-step1/signup-step1.component';
-import { SignupStep1Component } from 'src/app/components/signup-step1/signup-step1.component';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalController, NavController } from '@ionic/angular';
 import { LoginPage } from '../login/login.page';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { NgForm } from '@angular/forms';
 import { AlertService } from 'src/app/services/alert/alert.service';
-import { SignupStep2Component } from 'src/app/components/signup-step2/signup-step2.component';
+// import { SignupStep2Component } from 'src/app/components/signup-step2/signup-step2.component';
+// import { SignupStep1Component } from 'src/app/components/signup-step1/signup-step1.component';
 
 @Component({
   selector: 'app-register',
@@ -16,7 +14,8 @@ import { SignupStep2Component } from 'src/app/components/signup-step2/signup-ste
 export class RegisterPage implements OnInit {
 
   @ViewChild('signupSlider', {static: false}) signupSlider;
-  @ViewChild(SignupStep1Component, {static: false}) step1: SignupStep1Component;
+  // @ViewChild(SignupStep1Component, {static: false}) step1: SignupStep1Component;
+  // @ViewChild(SignupStep2Component, {static: false}) step2: SignupStep2Component;
 
   constructor(private modalController: ModalController,
               private authService: AuthService,
