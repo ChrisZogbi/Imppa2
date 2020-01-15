32b7ee7d203505cf8337730993468e7c139e556b
ChrisZogbi <chris.zogbi@gmail.com>
2020-01-15 12:42:34 -0300

    PuertoProductivo

--- api/app.js
+++ api/app.js
@@ -3,12 +3,13 @@ var app = express();
 import { json } from 'body-parser';
 import { assignRoutes } from './routes/index';
 import db from './services/index';
+var port=process.env.PORT || 3000;
 
 app.use(json());
 assignRoutes(app);
 
 
-var server = app.listen(3000, function () {
-    console.log('Server is running in 3000..');
+var server = app.listen(port, function () {
+    console.log('Server is running in' + port);
 });
 
